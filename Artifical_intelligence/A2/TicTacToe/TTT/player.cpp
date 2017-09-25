d2a7fd205d3f11c407101cdef11c61f419db3fc6
jiwidi <fhjaime96@gmail.com>
2017-09-25 13:43:33 +0200

    up

--- Artifical_intelligence/A2/TicTacToe/TTT/player.cpp
+++ Artifical_intelligence/A2/TicTacToe/TTT/player.cpp
@@ -4,13 +4,65 @@
 namespace TICTACTOE
 {
 
+int minimax(const GameState &pState, char Player)
+{
+    std::cerr << "Calculating minimax \n";
+    if (Player=='X')
+    {
+        int bestPossible = -99999;
+        std::vector<GameState> lNextStates;
+        pState.findPossibleMoves(lNextStates);
+        for(int i=0;i<lNextStates.size();i++){
+            if(lNextStates[i].isEOG()){
+                if(lNextStates[i].isXWin()){
+                    return +1;
+                }
+                else if(lNextStates[i].isOWin()){
+                    return -1;
+                }
+                else return 0;
+            }
+            int v=minimax(lNextStates[i],'O');
+            bestPossible=std::max(bestPossible,v);
+        }
+        return bestPossible;
+    }
+    else //Player='O'
+    {
+        int bestPossible = 99999;
+        std::vector<GameState> lNextStates;
+        pState.findPossibleMoves(lNextStates);
+        for(int i=0;i<lNextStates.size();i++){
+            if(lNextStates[i].isEOG()){
+                if(lNextStates[i].isXWin()){
+                    return -1;
+                }
+                else if(lNextStates[i].isOWin()){
+                    return 1;
+                }
+                else return 0;
+            }
+            int v=minimax(lNextStates[i],'X');
+            bestPossible=std::min(bestPossible,v);
+        }
+        return bestPossible;
+    }
+}   
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
     //std::cerr << "Processing " << pState.toMessage() << std::endl;
 
     std::vector<GameState> lNextStates;
     pState.findPossibleMoves(lNextStates);
+    GameState move=lNextStates[rand() % lNextStates.size()];
+    int v=0;
+    for(int i=0;i<lNextStates.size();i++){
+        if(minimax(lNextStates[i],'X')>v){
+            move=lNextStates[i];
+            v=minimax(lNextStates[i],'X');
+        }
 
+    }
 
     if (lNextStates.size() == 0) return GameState(pState, Move());
 
@@ -18,7 +70,10 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
      * Here you should write your clever algorithms to get the best next move, ie the best
      * next state. This skeleton returns a random move instead.
      */
-    return lNextStates[rand() % lNextStates.size()];
+
+    return move;
 }
+
+
 
 /*namespace TICTACTOE*/ }
