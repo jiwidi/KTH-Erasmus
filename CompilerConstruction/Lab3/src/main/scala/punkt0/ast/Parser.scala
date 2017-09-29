01715d3ff69df5eebfcdc35b46a3b8c22af230b5
Per Hellström <perhells@kth.se>
2017-09-29 12:28:20 +0200

    Basic parser, skipping main and classes

--- CompilerConstruction/Lab3/src/main/scala/punkt0/ast/Parser.scala
+++ CompilerConstruction/Lab3/src/main/scala/punkt0/ast/Parser.scala
@@ -3,6 +3,7 @@ package ast
 
 import Trees._
 import lexer._
+import scala.collection.mutable.ListBuffer
 
 object Parser extends Phase[Iterator[Token], Program] {
   def run(tokens: Iterator[Token])(ctx: Context): Program = {
@@ -22,6 +23,17 @@ object Parser extends Phase[Iterator[Token], Program] {
       }
     }
 
+    def expect(kind : TokenKind): Token = {
+      if (currentToken.kind == kind) {
+        var token = currentToken
+        readToken
+        token
+      } else {
+        error("Expected " + kind)
+        currentToken
+      }
+    }
+
     /** ''Eats'' the expected token, or terminates with an error. */
     def eat(kind: TokenKind): Unit = {
       if (currentToken.kind == kind) {
@@ -37,11 +49,68 @@ object Parser extends Phase[Iterator[Token], Program] {
     }
 
     def parseGoal: Program = {
-      ???
+      var classes = new ListBuffer[ClassDecl]
+      while (currentToken.kind == CLASS) {
+        classes += parseClass
+      }
+      var main = parseMain
+      Program(main, classes.toList)
+    }
+
+    def parseMain: MainDecl = {
+      eat(OBJECT)
+      val identifier = expect(IDKIND).toString
+      eat(EXTENDS)
+      val parent = expect(IDKIND).toString
+      eat(LBRACE)
+      var vars = new ListBuffer[VarDecl]
+      var exprs = new ListBuffer[ExprTree]
+      // Temp skip, removes everything until matching brace
+      var depth = 0
+      while (currentToken.kind != RBRACE || depth > 0) {
+        if (currentToken.kind == LBRACE) {
+          depth += 1
+        } else if (currentToken.kind == RBRACE) {
+          depth -= 1
+        }
+        readToken
+      }
+      // Parse main method
+      eat(RBRACE)
+      MainDecl(Identifier(identifier), Identifier(parent), vars.toList, exprs.toList)
+    }
+
+    def parseClass: ClassDecl = {
+      eat(CLASS)
+      val identifier = expect(IDKIND).toString
+      var parent: Option[Identifier] = None
+      if (currentToken.kind == EXTENDS) {
+        eat(EXTENDS)
+        parent = Some(Identifier(expect(IDKIND).toString))
+      }
+      eat(LBRACE)
+      var vars = new ListBuffer[VarDecl]
+      var methods = new ListBuffer[MethodDecl]
+      // Temp skip, removes everything until matching brace
+      var depth = 0
+      while (currentToken.kind != RBRACE || depth > 0) {
+        if (currentToken.kind == LBRACE) {
+          depth += 1
+        } else if (currentToken.kind == RBRACE) {
+          depth -= 1
+        }
+        readToken
+      }
+      // Parse class
+      eat(RBRACE)
+      ClassDecl(Identifier(identifier), parent, vars.toList, methods.toList)
     }
 
     readToken
     val tree = parseGoal
+    if (ctx.doAST) {
+      println(tree)
+    }
     terminateIfErrors
     tree
   }
