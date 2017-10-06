b035db4162f29bd0a8d31f5bb68a518d90f1c4da
jiwidi <fhjaime96@gmail.com>
2017-10-06 13:14:25 +0200

    lab1 done start lab2

--- Machine_Learning/lab1/dectrees/python/q6.py
+++ Machine_Learning/lab1/dectrees/python/q6.py
@@ -29,18 +29,18 @@ monk1simple=[]
 monk3simple=[]
 
 for v in fraction:
-    monk1train,monk1test=partition(m.monk1,v)
-    monk3train,monk3test=partition(m.monk3,v)
+    monk1train,monk1validation=partition(m.monk1,v)
+    monk3train,monk3validation=partition(m.monk3,v)
     monk1tree=d.buildTree(monk1train,m.attributes)
     monk3tree=d.buildTree(monk3train,m.attributes)
-    monk1simple.append(d.check(monk1tree,monk1test))
-    monk3simple.append(d.check(monk3tree,monk3test))
+    monk1simple.append(d.check(monk1tree,monk1validation))
+    monk3simple.append(d.check(monk3tree,monk3validation))
 
 #Monk1 evaluation
 for v in fraction:
     value=0
     for _ in range(reps):
-        monk1train,monk1test=partition(m.monk1,v)
+        monk1train,monk1validation=partition(m.monk1,v)
         monk1tree=d.buildTree(monk1train,m.attributes)
         topPerformance=0
         performance=0
@@ -49,12 +49,12 @@ for v in fraction:
             keepPruning=False
             pruned=d.allPruned(monk1tree)
             for tree in pruned:
-                performance=d.check(tree,monk1test)
+                performance=d.check(tree,monk1validation)
                 if(performance>topPerformance):
                     keepPruning=True
                     topPerformance=performance
                     monk1tree=tree
-        valuesmonk1.append(d.check(monk1tree,monk1test))
+        valuesmonk1.append(d.check(monk1tree,m.monk1test))
     print("Monk1 fraction "+ str(v))
     mean=np.mean(valuesmonk1)
     print("    Error: "+str(1-mean))
@@ -70,7 +70,7 @@ print()
 for v in fraction:
     value=0
     for _ in range(reps):
-        monk3train,monk3test=partition(m.monk3,v)
+        monk3train,monk3validation=partition(m.monk3,v)
         monk3tree=d.buildTree(monk3train,m.attributes)
         topPerformance=0
         performance=0
@@ -79,12 +79,12 @@ for v in fraction:
             keepPruning=False
             pruned=d.allPruned(monk3tree)
             for tree in pruned:
-                performance=d.check(tree,monk3test)
+                performance=d.check(tree,monk3validation)
                 if(performance>topPerformance):
                     keepPruning=True
                     topPerformance=performance
                     monk3train=tree
-        valuesmonk3.append(d.check(monk3tree,monk3test))
+        valuesmonk3.append(d.check(monk3tree,m.monk3test))
     print("Monk3 fraction "+ str(v))
     mean=np.mean(valuesmonk3)
     print("    Error: "+str(1-mean))
@@ -130,17 +130,26 @@ trace3 = go.Scatter(
     fillcolor='rgba(0,100,80,0.2)',
     connectgaps=True
 )
-data =[trace4,trace3]
+# data =[trace4,trace3] #Variance
+data =[trace1,trace2] #Error
 # Edit the layout
+
 layout = dict(title = 'Effect of different prunings fractions in monk1 and monk3',
-              xaxis = dict(title = 'Fraction between train and test'),
+              xaxis = dict(title = 'Fraction between train and validation datasets'),
               yaxis = dict(title = 'Variance'),
               )
 
 fig = dict(data=data, layout=layout)
-plotly.plotly.iplot(fig, filename='Q7 ML lab1 Variance ')
+plotly.plotly.iplot(fig, filename='Q7 ML lab1 Error test v3 ')
 
+data =[trace4,trace3] #Variance
+layout = dict(title = 'Effect of different prunings fractions in monk1 and monk3',
+              xaxis = dict(title = 'Fraction between train and test'),
+              yaxis = dict(title = 'Error on test dataset'),
+              )
 
+fig = dict(data=data, layout=layout)
+plotly.plotly.iplot(fig, filename='Q7 ML lab1 variance v3 ')
 
 
 
