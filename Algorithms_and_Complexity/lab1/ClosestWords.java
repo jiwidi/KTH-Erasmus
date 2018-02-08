90b0a89145a305d3d623af352f5b1da10cda7335
jaime <fhjaime96@gmail.com>
2018-02-08 09:22:01 +0100

    Lab1
    update

--- Algorithms_and_Complexity/lab1/ClosestWords.java
+++ Algorithms_and_Complexity/lab1/ClosestWords.java
@@ -9,24 +9,59 @@ public class ClosestWords {
 
   int closestDistance = -1;
 
-  int partDist(String w1, String w2, int w1len, int w2len) {
+   public static int partDist(String w1, String w2, int w1len, int w2len,int[][] M) {
+       
     if (w1len == 0)
       return w2len;
     if (w2len == 0)
       return w1len;
-    int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
+    if (M[w1len][w2len]!=-1){
+        return M[w1len][w2len];
+    }
+    int res = partDist(w1, w2, w1len - 1, w2len - 1,M) + 
 	(w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-    int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
+    int addLetter = partDist(w1, w2, w1len - 1, w2len,M) + 1;
     if (addLetter < res)
       res = addLetter;
-    int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
+    int deleteLetter = partDist(w1, w2, w1len, w2len - 1,M) + 1;
     if (deleteLetter < res)
       res = deleteLetter;
     return res;
-  }
+   }
+  
+   public static int fill(String w1, String w2, int w1len, int w2len,int[][] M){
+      int ci,cj;
+      ci=0;
+      cj=0;
+      for (int i = 0; i < M.length; i++) {
+          for (int j = 0; j < M[i].length; j++) {
+              M[i][j]=partDist(w1,w2,ci,cj,M);
+              cj++;
+          }
+          ci++;
+          cj=0;
+          System.out.println();
+      } 
+      for (int i = 0; i < M.length; i++) {
+          for (int j = 0; j < M[i].length; j++) {
+              System.out.print(M[i][j] + " ");
+          }
+          System.out.println();
+      }
+      return M[w1len][w2len];
+   }
 
-  int Distance(String w1, String w2) {
-    return partDist(w1, w2, w1.length(), w2.length());
+  public static int Distance(String w1, String w2) {   
+      int[][] M = new int[w1.length()+1][w2.length()+1];
+      for (int i = 0; i < M.length; i++) {
+          for (int j = 0; j < M[i].length; j++) {
+              M[i][j]=-1;
+          }
+          System.out.println();
+      }
+      return fill(w1, w2, w1.length(), w2.length(),M);
+      
+
   }
 
   public ClosestWords(String w, List<String> wordList) {
