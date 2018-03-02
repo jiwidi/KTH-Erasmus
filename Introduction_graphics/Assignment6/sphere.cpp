4ac2a808d6a0831b794d88e87e16c4b95f31683a
Sonia Camacho <32477297+sonia-ch@users.noreply.github.com>
2018-03-02 11:35:49 +0100

    assignment 6 tasks 1 and 2

---
+++ Introduction_graphics/Assignment6/sphere.cpp
@@ -0,0 +1,116 @@
+/*********************************************************************
+ *  Author  : Himangshu Saikia
+ *  Init    : Tuesday, October 17, 2017 - 10:24:56
+ *
+ *  Project : KTH Inviwo Modules
+ *
+ *  License : Follows the Inviwo BSD license model
+ *********************************************************************
+ */
+
+#include <labraytracer/sphere.h>
+#include <labraytracer/util.h>
+
+namespace inviwo {
+
+Sphere::Sphere(const vec3& center, const double& radius) {
+    center_ = center;
+    radius_ = radius;
+}
+
+
+/*
+* 
+*
+*
+*/
+bool Sphere::closestIntersection(const Ray& ray, double maxLambda,
+                                 RayIntersection& intersection) const {
+    //Programming TASK 1: implement this method
+    //Your code should compute the intersection between a ray and a sphere;
+
+    vec3 L = center_ - ray.getOrigin();
+    double t_od = dot(L, ray.getDirection());
+    
+    if(t_od < 0) return false;
+
+    double L_len = sqrt(L.x * L.x + L.y * L.y + L.z * L.z);
+
+    double d = sqrt((L_len * L_len) - (t_od * t_od));
+
+    if(d > radius_) return false;
+
+    double t_hc = sqrt((radius_*radius_) - (d*d));
+
+    double t0 = t_od - t_hc; // This is closest
+    double t1 = t_od + t_hc;
+    double tUse = t0;
+
+    if(tUse >= maxLambda) return false;
+
+    const vec3 uvw(0, 0, 0);
+    intersection = RayIntersection(
+        ray,
+        shared_from_this(),
+        tUse,
+        Util::normalize(ray.pointOnRay(tUse)-center_),
+        uvw);
+    return true;
+
+    //If you detect an intersection, the return type should look similar to this:
+    //if(rayIntersectsSphere)
+    //{
+    //  intersection = RayIntersection(ray,shared_from_this(),lambda,ray.pointOnRay(lambda),uvw);
+    //  return true;
+    //}
+    //
+    // Hints :
+    // Ray origin p_r : ray.getOrigin()
+    // Ray direction t_r : ray.getDirection()
+    // Compute the intersection point using ray.pointOnRay(lambda)
+
+    // • const Ray &ray: constant reference of the Ray with which an intersection is found.
+    // • std::shared ptr<const Renderable> renderable: Reference in the form of a smart pointer to the Object
+    // with which the intersection occurred. Use the term shared_from_this() here.
+    // • const real lambda: Ray parameter where λ ∈ [0, maxLambda].
+    // • const Vec3d &normal: constant reference to the normal direction at the point of intersection on the
+    // Object’s surface.
+    // • const Vec3d &uvw: ignore this and assign the Null Vector (0, 0, 0).
+    
+    // return false;
+}
+
+bool Sphere::anyIntersection(const Ray& ray, double maxLambda) const {
+    RayIntersection temp;
+    return closestIntersection(ray, maxLambda, temp);
+}
+
+void Sphere::drawGeometry(std::shared_ptr<BasicMesh> mesh,
+                          std::vector<BasicMesh::Vertex>& vertices) const {
+    auto indexBuffer = mesh->addIndexBuffer(DrawType::Lines, ConnectivityType::None);
+
+    int lat = 8;
+    int lon = 10;
+
+    for (int i = 0; i < lat - 1; i++) {
+        float theta1 = float(i * M_PI) / (lat - 1);
+        float theta2 = float((i + 1) * M_PI) / (lat - 1);
+
+        for (int j = 0; j < lon - 1; j++) {
+            float phi1 = float(j * 2 * M_PI) / (lon - 1);
+            float phi2 = float((j + 1) * 2 * M_PI) / (lon - 1);
+
+            vec3 v1 = vec3(radius_ * sin(theta1) * cos(phi1), radius_ * sin(theta1) * sin(phi1),
+                           radius_ * cos(theta1)) + center_;
+            vec3 v2 = vec3(radius_ * sin(theta2) * cos(phi1), radius_ * sin(theta2) * sin(phi1),
+                           radius_ * cos(theta2)) + center_;
+            vec3 v3 = vec3(radius_ * sin(theta2) * cos(phi2), radius_ * sin(theta2) * sin(phi2),
+                           radius_ * cos(theta2)) + center_;
+
+            Util::drawLineSegment(v1, v2, vec4(0.2, 0.2, 0.2, 1), indexBuffer, vertices);
+            Util::drawLineSegment(v2, v3, vec4(0.2, 0.2, 0.2, 1), indexBuffer, vertices);
+        }
+    }
+}
+
+} // namespace
---
+++ Introduction_graphics/Assignment6/triangle.cpp
@@ -0,0 +1,93 @@
+/*********************************************************************
+ *  Author  : Himangshu Saikia
+ *  Init    : Tuesday, October 17, 2017 - 10:24:30
+ *
+ *  Project : KTH Inviwo Modules
+ *
+ *  License : Follows the Inviwo BSD license model
+ *********************************************************************
+ */
+
+#include <labraytracer/triangle.h>
+#include <labraytracer/util.h>
+#include <memory>
+
+namespace inviwo {
+
+Triangle::Triangle() {
+}
+
+Triangle::Triangle(const vec3& v0, const vec3& v1, const vec3& v2, const vec3& uvw0,
+                   const vec3& uvw1, const vec3& uvw2) {
+    mVertices[0] = v0;
+    mVertices[1] = v1;
+    mVertices[2] = v2;
+    mUVW[0] = uvw0;
+    mUVW[1] = uvw1;
+    mUVW[2] = uvw2;
+}
+
+bool Triangle::closestIntersection(const Ray& ray, double maxLambda,
+                                   RayIntersection& intersection) const {
+    
+	vec3 v0v1 = mVertices[1] - mVertices[0];
+	vec3 v0v2 = mVertices[2] - mVertices[0];
+	vec3 pvec = cross(ray.getDirection(), v0v2);
+	float det = dot(v0v1, pvec);
+	det = 1 / det;
+
+	vec3 tvec = ray.getOrigin() - mVertices[0];
+	double u = dot(tvec, pvec) * det;
+	if (u < 0 || u > 1) return false;
+
+	vec3 qvec = cross(tvec, v0v1);
+	double v = dot(ray.getDirection(), qvec) * det;
+	if (v < 0 || u + v > 1) return false;
+
+	double t = dot(v0v2, qvec) * det;
+	if (t < 0 || t + Util::epsilon > maxLambda) {
+		return false;
+	}
+
+    vec3 normal = cross(v0v1, v0v2);
+
+	const vec3 uvw(0, 0, 0);
+	intersection = RayIntersection(ray, shared_from_this(), t, normal, uvw);
+
+
+	return true;
+
+    //Programming TASK 1: Implement this method
+    //Your code should compute the intersection between a ray and a triangle.
+    //
+    //If you detect an intersection, the return type should look similar to this:
+    //if(rayIntersectsTriangle)
+    //{
+    //  intersection = RayIntersection(ray,shared_from_this(),lambda,ray.normal,uvw);
+    //  return true;
+    //} 
+    //
+    // Hints :
+    // Ray origin p_r : ray.getOrigin()
+    // Ray direction t_r : ray.getDirection()
+    // Compute the intersection point using ray.pointOnRay(lambda) 
+}
+
+bool Triangle::anyIntersection(const Ray& ray, double maxLambda) const {
+    RayIntersection temp;
+    return closestIntersection(ray, maxLambda, temp);
+}
+
+void Triangle::drawGeometry(std::shared_ptr<BasicMesh> mesh,
+                            std::vector<BasicMesh::Vertex>& vertices) const {
+    auto indexBuffer = mesh->addIndexBuffer(DrawType::Lines, ConnectivityType::None);
+
+    Util::drawLineSegment(mVertices[0], mVertices[1], vec4(0.2, 0.2, 0.2, 1), indexBuffer,
+                          vertices);
+    Util::drawLineSegment(mVertices[1], mVertices[2], vec4(0.2, 0.2, 0.2, 1), indexBuffer,
+                          vertices);
+    Util::drawLineSegment(mVertices[2], mVertices[0], vec4(0.2, 0.2, 0.2, 1), indexBuffer,
+                          vertices);
+}
+
+} // namespace
